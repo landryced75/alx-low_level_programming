5031bd2e481c3e14fda2c89a1f3dd207b220070b
landryced75 <99596328+landryced75@users.noreply.github.com>
2022-04-12 19:08:15 +0200

    delware

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - A function that creates an array of chars,
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A function that creates an array of chars,
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
